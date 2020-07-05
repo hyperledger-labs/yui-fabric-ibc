685ab463eb2f00a080c0ebcfc7ee7fe3bad6c90e
toshihiko-okubo <toshihiko.okubo@speee.jp>
2020-07-06 02:20:34 +0900

    remove dependency on Local cinfigtxgen binary

--- dev/sh/init.sh
+++ dev/sh/init.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 set -eux
 
+ORG_NAME=SampleOrg
 CHANNEL_NAME=dev
 export FABRIC_CFG_PATH=${PWD}/config
 
-configtxgen -profile OrdererGenesis -channelID system-channel -outputBlock ./artifacts/orderer.block
-configtxgen -profile SampleSingleMSPSolo -outputCreateChannelTx ./artifacts/${CHANNEL_NAME}.tx -channelID ${CHANNEL_NAME}
-configtxgen -profile SampleSingleMSPSolo -outputAnchorPeersUpdate ./artifacts/SampleOrgAnchors.tx -channelID ${CHANNEL_NAME} -asOrg SampleOrg
+configtxgen \
+-profile OrdererGenesis \
+-channelID system-channel \
+-outputBlock ./artifacts/orderer.block
+
+configtxgen \
+-profile SampleSingleMSPSolo \
+-channelID ${CHANNEL_NAME} \
+-outputCreateChannelTx ./artifacts/${CHANNEL_NAME}.tx
+
+configtxgen \
+-profile SampleSingleMSPSolo \
+-channelID ${CHANNEL_NAME} \
+-outputAnchorPeersUpdate ./artifacts/${ORG_NAME}Anchors.tx \
+-asOrg ${ORG_NAME}
