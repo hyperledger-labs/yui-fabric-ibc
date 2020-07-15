adde1047ec331ae95e6f59e772340eeac4438fd3
Jun Kimura <junkxdev@gmail.com>
2020-07-15 17:14:04 +0900

    move auth package into x/auth
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- x/auth/ante/ante.go
+++ x/auth/ante/ante.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/datachainlab/fabric-ibc/auth/types"
+	"github.com/datachainlab/fabric-ibc/x/auth/types"
 	"github.com/hyperledger/fabric-chaincode-go/pkg/cid"
 )
 
