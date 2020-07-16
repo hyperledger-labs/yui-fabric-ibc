65815b5b830e342547bc1c29b42048a2af2f1ace
Jun Kimura <junkxdev@gmail.com>
2020-07-16 17:16:53 +0900

    set MessageProof type to codec

--- x/ibc/xx-fabric/types/codec.go
+++ x/ibc/xx-fabric/types/codec.go
@@ -8,6 +8,7 @@ import (
 // ModuleCdc defines the IBC client codec.
 var ModuleCdc *codec.Codec
 
+// RegisterCodec registers concrete types and interfaces on the given codec.
 func RegisterCodec(cdc *codec.Codec) {
 	// msg types
 	cdc.RegisterConcrete(MsgCreateClient{}, "ibc/fabric/MsgCreateClient", nil)
@@ -22,6 +23,7 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(ChaincodeHeader{}, "ibc/fabric/types/ChaincodeHeader", nil)
 	cdc.RegisterConcrete(ChaincodeInfo{}, "ibc/fabric/types/ChaincodeInfo", nil)
 	cdc.RegisterConcrete(CommitmentProof{}, "ibc/fabric/types/CommitmentProof", nil)
+	cdc.RegisterConcrete(MessageProof{}, "ibc/fabric/types/MessageProof", nil)
 	cdc.RegisterConcrete(peer.ChaincodeID{}, "ibc/fabric/types/ChaincodeID", nil)
 }
 
