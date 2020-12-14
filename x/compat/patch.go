b400fbffa4afad6fca58d270298955b14c6e013c
Jun Kimura <junkxdev@gmail.com>
2020-12-14 17:53:53 +0900

    add patch function
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

---
+++ x/compat/patch.go
@@ -0,0 +1,18 @@
+package compat
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	connectionkeeper "github.com/cosmos/cosmos-sdk/x/ibc/core/03-connection/keeper"
+	ibckeeper "github.com/cosmos/cosmos-sdk/x/ibc/core/keeper"
+	"github.com/datachainlab/fabric-ibc/commitment"
+	fabrickeeper "github.com/datachainlab/fabric-ibc/x/ibc/light-clients/xx-fabric/keeper"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+)
+
+// ApplyPatchToIBCKeeper applies patches to ibc keeper
+func ApplyPatchToIBCKeeper(k ibckeeper.Keeper, cdc codec.BinaryMarshaler, key sdk.StoreKey, stub shim.ChaincodeStubInterface, seqMgr *commitment.SequenceManager) ibckeeper.Keeper {
+	clientKeeper := fabrickeeper.NewClientKeeper(k.ClientKeeper, stub, seqMgr)
+	k.ConnectionKeeper = connectionkeeper.NewKeeper(cdc, key, clientKeeper)
+	return k
+}
