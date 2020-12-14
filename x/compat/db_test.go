d48074ab7ae1fdbc9dbb1d6e631cb260a3350be8
Jun Kimura <junkxdev@gmail.com>
2020-12-14 18:33:07 +0900

    fix compatible DB
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- x/compat/db_test.go
+++ x/compat/db_test.go
@@ -23,47 +23,98 @@ func TestBackendGetSetDelete(t *testing.T) {
 }
 
 func testBackendGetSetDelete(t *testing.T, db db.DB) {
-	// A nonexistent key should return nil, even if the key is empty
-	item, err := db.Get([]byte(""))
-	require.NoError(t, err)
-	require.Nil(t, item)
-
-	// A nonexistent key should return nil, even if the key is nil
-	value, err := db.Get(nil)
-	require.NoError(t, err)
-	require.Nil(t, value)
-
 	// A nonexistent key should return nil.
-	key := []byte("abc")
-	value, err = db.Get(key)
+	value, err := db.Get([]byte("a"))
 	require.NoError(t, err)
 	require.Nil(t, value)
 
-	// Set empty value.
-	err = db.Set(key, []byte(""))
+	ok, err := db.Has([]byte("a"))
+	require.NoError(t, err)
+	require.False(t, ok)
+
+	// Set and get a value.
+	err = db.Set([]byte("a"), []byte{0x01})
 	require.NoError(t, err)
 
-	value, err = db.Get(key)
+	ok, err = db.Has([]byte("a"))
 	require.NoError(t, err)
-	require.NotNil(t, value)
-	require.Empty(t, value)
+	require.True(t, ok)
 
-	// Set nil value.
-	err = db.Set(key, nil)
+	value, err = db.Get([]byte("a"))
 	require.NoError(t, err)
+	require.Equal(t, []byte{0x01}, value)
 
-	value, err = db.Get(key)
-	require.NoError(t, err)
-	require.NotNil(t, value)
-	require.Empty(t, value)
-
-	// Delete.
-	err = db.Delete(key)
+	err = db.SetSync([]byte("b"), []byte{0x02})
 	require.NoError(t, err)
 
-	value, err = db.Get(key)
+	value, err = db.Get([]byte("b"))
+	require.NoError(t, err)
+	require.Equal(t, []byte{0x02}, value)
+
+	// Deleting a non-existent value is fine.
+	err = db.Delete([]byte("x"))
+	require.NoError(t, err)
+
+	err = db.DeleteSync([]byte("x"))
+	require.NoError(t, err)
+
+	// Delete a value.
+	err = db.Delete([]byte("a"))
+	require.NoError(t, err)
+
+	value, err = db.Get([]byte("a"))
 	require.NoError(t, err)
 	require.Nil(t, value)
+
+	err = db.DeleteSync([]byte("b"))
+	require.NoError(t, err)
+
+	value, err = db.Get([]byte("b"))
+	require.NoError(t, err)
+	require.Nil(t, value)
+
+	// Setting, getting, and deleting an empty key should error.
+	_, err = db.Get([]byte{})
+	require.Equal(t, errKeyEmpty, err)
+	_, err = db.Get(nil)
+	require.Equal(t, errKeyEmpty, err)
+
+	_, err = db.Has([]byte{})
+	require.Equal(t, errKeyEmpty, err)
+	_, err = db.Has(nil)
+	require.Equal(t, errKeyEmpty, err)
+
+	err = db.Set([]byte{}, []byte{0x01})
+	require.Equal(t, errKeyEmpty, err)
+	err = db.Set(nil, []byte{0x01})
+	require.Equal(t, errKeyEmpty, err)
+	err = db.SetSync([]byte{}, []byte{0x01})
+	require.Equal(t, errKeyEmpty, err)
+	err = db.SetSync(nil, []byte{0x01})
+	require.Equal(t, errKeyEmpty, err)
+
+	err = db.Delete([]byte{})
+	require.Equal(t, errKeyEmpty, err)
+	err = db.Delete(nil)
+	require.Equal(t, errKeyEmpty, err)
+	err = db.DeleteSync([]byte{})
+	require.Equal(t, errKeyEmpty, err)
+	err = db.DeleteSync(nil)
+	require.Equal(t, errKeyEmpty, err)
+
+	// Setting a nil value should error, but an empty value is fine.
+	err = db.Set([]byte("x"), nil)
+	require.Equal(t, errValueNil, err)
+	err = db.SetSync([]byte("x"), nil)
+	require.Equal(t, errValueNil, err)
+
+	err = db.Set([]byte("x"), []byte{})
+	require.NoError(t, err)
+	err = db.SetSync([]byte("x"), []byte{})
+	require.NoError(t, err)
+	value, err = db.Get([]byte("x"))
+	require.NoError(t, err)
+	require.Equal(t, []byte{}, value)
 }
 
 func makeStubBasedDB() *DB {
@@ -71,138 +122,15 @@ func makeStubBasedDB() *DB {
 	return NewDB(stub)
 }
 
-func TestBackendsNilKeys(t *testing.T) {
+func TestDBIterator(t *testing.T) {
 	backends := []db.DB{
 		db.NewMemDB(),
 		makeStubBasedDB(),
 	}
-
-	// Test all backends.
 	for i, db := range backends {
-		t.Run(fmt.Sprintf("Testing %v", i), func(t *testing.T) {
-			expect := func(key, value []byte) {
-				if len(key) == 0 { // nil or empty
-					nilValue, err := db.Get(nil)
-					assert.NoError(t, err)
-					byteValue, err := db.Get([]byte(""))
-					assert.NoError(t, err)
-					assert.Equal(t, nilValue, byteValue)
-					hasNil, err := db.Has(nil)
-					assert.NoError(t, err)
-					hasStr, err := db.Has([]byte(""))
-					assert.NoError(t, err)
-					assert.Equal(t, hasNil, hasStr)
-				}
-				value2, err := db.Get(key)
-				assert.Equal(t, value2, value)
-				assert.NoError(t, err)
-				hasKey, err := db.Has(key)
-				assert.NoError(t, err)
-				assert.Equal(t, hasKey, value != nil)
-			}
-
-			// Not set
-			expect(nil, nil)
-
-			// Set nil value
-			err := db.Set(nil, nil)
-			require.NoError(t, err)
-			expect(nil, []byte(""))
-
-			// Set empty value
-			err = db.Set(nil, []byte(""))
-			require.NoError(t, err)
-			expect(nil, []byte(""))
-
-			// Set nil, Delete nil
-			err = db.Set(nil, []byte("abc"))
-			expect(nil, []byte("abc"))
-			require.NoError(t, err)
-			err = db.Delete(nil)
-			require.NoError(t, err)
-			expect(nil, nil)
-
-			// Set nil, Delete empty
-			err = db.Set(nil, []byte("abc"))
-			expect(nil, []byte("abc"))
-			require.NoError(t, err)
-			err = db.Delete([]byte(""))
-			require.NoError(t, err)
-			expect(nil, nil)
-
-			// Set empty, Delete nil
-			err = db.Set([]byte(""), []byte("abc"))
-			expect(nil, []byte("abc"))
-			require.NoError(t, err)
-			err = db.Delete(nil)
-			require.NoError(t, err)
-			expect(nil, nil)
-
-			// Set empty, Delete empty
-			err = db.Set([]byte(""), []byte("abc"))
-			require.NoError(t, err)
-			expect(nil, []byte("abc"))
-
-			err = db.Delete([]byte(""))
-			require.NoError(t, err)
-			expect(nil, nil)
-
-			// SetSync nil, DeleteSync nil
-			err = db.SetSync(nil, []byte("abc"))
-			require.NoError(t, err)
-			expect(nil, []byte("abc"))
-			err = db.DeleteSync(nil)
-			require.NoError(t, err)
-			expect(nil, nil)
-
-			// SetSync nil, DeleteSync empty
-			err = db.SetSync(nil, []byte("abc"))
-			require.NoError(t, err)
-			err = db.DeleteSync([]byte(""))
-			require.NoError(t, err)
-
-			// SetSync empty, DeleteSync nil
-			err = db.SetSync([]byte(""), []byte("abc"))
-			require.NoError(t, err)
-			expect(nil, []byte("abc"))
-			err = db.DeleteSync(nil)
-			require.NoError(t, err)
-			expect(nil, nil)
-
-			// SetSync empty, DeleteSync empty
-			err = db.SetSync([]byte(""), []byte("abc"))
-			require.NoError(t, err)
-			expect(nil, []byte("abc"))
-			err = db.DeleteSync([]byte(""))
-			require.NoError(t, err)
-			expect(nil, nil)
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			testDBIterator(t, db)
 		})
-	}
-}
-
-func TestDBIterator(t *testing.T) {
-	{
-		backends := []db.DB{
-			db.NewMemDB(),
-			makeStubBasedDB(),
-		}
-		for i, db := range backends {
-			t.Run(fmt.Sprint(i), func(t *testing.T) {
-				testDBIterator(t, db)
-			})
-		}
-	}
-
-	{
-		backends := []db.DB{
-			db.NewMemDB(),
-			makeStubBasedDB(),
-		}
-		for i, db := range backends {
-			t.Run(fmt.Sprint(i), func(t *testing.T) {
-				testDBIteratorBlankKey(t, db)
-			})
-		}
 	}
 }
 
@@ -210,10 +138,21 @@ func TestDBIterator(t *testing.T) {
 func testDBIterator(t *testing.T, db db.DB) {
 	for i := 0; i < 10; i++ {
 		if i != 6 { // but skip 6.
-			err := db.Set(int642Bytes(int64(i)), nil)
+			err := db.Set(int642Bytes(int64(i)), []byte{})
 			require.NoError(t, err)
 		}
 	}
+	var err error
+
+	// Blank iterator keys should error
+	_, err = db.Iterator([]byte{}, nil)
+	require.Equal(t, errKeyEmpty, err)
+	_, err = db.Iterator(nil, []byte{})
+	require.Equal(t, errKeyEmpty, err)
+	_, err = db.ReverseIterator([]byte{}, nil)
+	require.Equal(t, errKeyEmpty, err)
+	_, err = db.ReverseIterator(nil, []byte{})
+	require.Equal(t, errKeyEmpty, err)
 
 	itr, err := db.Iterator(nil, nil)
 	require.NoError(t, err)
@@ -317,14 +256,9 @@ func testDBIterator(t *testing.T, db db.DB) {
 	require.NoError(t, err)
 	verifyIterator(t, ritr, []int64{9, 8, 7, 5}, "reverse iterator to 5")
 
-	// verifyIterator(t, db.Iterator(int642Bytes(0), int642Bytes(1)), []int64{0}, "forward iterator from 0 to 1")
-
 	ritr, err = db.ReverseIterator(int642Bytes(8), int642Bytes(9))
 	require.NoError(t, err)
 	verifyIterator(t, ritr, []int64{8}, "reverse iterator from 9 (ex) to 8")
-
-	// verifyIterator(t, db.Iterator(int642Bytes(2), int642Bytes(4)), []int64{2, 3}, "forward iterator from 2 to 4")
-	// verifyIterator(t, db.Iterator(int642Bytes(4), int642Bytes(2)), []int64(nil), "forward iterator from 4 to 2")
 
 	ritr, err = db.ReverseIterator(int642Bytes(2), int642Bytes(4))
 	require.NoError(t, err)
@@ -335,51 +269,6 @@ func testDBIterator(t *testing.T, db db.DB) {
 	require.NoError(t, err)
 	verifyIterator(t, ritr,
 		[]int64(nil), "reverse iterator from 2 (ex) to 4")
-}
-
-func testDBIteratorBlankKey(t *testing.T, db db.DB) {
-	err := db.Set([]byte(""), []byte{0})
-	require.NoError(t, err)
-	err = db.Set([]byte("a"), []byte{1})
-	require.NoError(t, err)
-	err = db.Set([]byte("b"), []byte{2})
-	require.NoError(t, err)
-
-	value, err := db.Get([]byte(""))
-	require.NoError(t, err)
-	assert.Equal(t, []byte{0}, value)
-
-	i, err := db.Iterator(nil, nil)
-	require.NoError(t, err)
-	verifyIteratorStrings(t, i, []string{"", "a", "b"}, "forward")
-
-	i, err = db.Iterator([]byte(""), nil)
-	require.NoError(t, err)
-	verifyIteratorStrings(t, i, []string{"", "a", "b"}, "forward from blank")
-
-	i, err = db.Iterator([]byte("a"), nil)
-	require.NoError(t, err)
-	verifyIteratorStrings(t, i, []string{"a", "b"}, "forward from a")
-
-	i, err = db.Iterator([]byte(""), []byte("b"))
-	require.NoError(t, err)
-	verifyIteratorStrings(t, i, []string{"", "a"}, "forward from blank to b")
-
-	i, err = db.ReverseIterator(nil, nil)
-	require.NoError(t, err)
-	verifyIteratorStrings(t, i, []string{"b", "a", ""}, "reverse")
-
-	i, err = db.ReverseIterator([]byte(""), nil)
-	require.NoError(t, err)
-	verifyIteratorStrings(t, i, []string{"b", "a", ""}, "reverse to blank")
-
-	i, err = db.ReverseIterator([]byte(""), []byte("a"))
-	require.NoError(t, err)
-	verifyIteratorStrings(t, i, []string{""}, "reverse to blank from a")
-
-	i, err = db.ReverseIterator([]byte("a"), nil)
-	require.NoError(t, err)
-	verifyIteratorStrings(t, i, []string{"b", "a"}, "reverse to a")
 }
 
 func TestDBBatch(t *testing.T) {
@@ -397,50 +286,50 @@ func TestDBBatch(t *testing.T) {
 func testDBBatch(t *testing.T, db db.DB) {
 	// create a new batch, and some items - they should not be visible until we write
 	batch := db.NewBatch()
-	batch.Set([]byte("a"), []byte{1})
-	batch.Set([]byte("b"), []byte{2})
-	batch.Set([]byte("c"), []byte{3})
+	require.NoError(t, batch.Set([]byte("a"), []byte{1}))
+	require.NoError(t, batch.Set([]byte("b"), []byte{2}))
+	require.NoError(t, batch.Set([]byte("c"), []byte{3}))
 	assertKeyValues(t, db, map[string][]byte{})
 
 	err := batch.Write()
 	require.NoError(t, err)
 	assertKeyValues(t, db, map[string][]byte{"a": {1}, "b": {2}, "c": {3}})
 
-	// trying to modify or rewrite a written batch should panic, but closing it should work
-	require.Panics(t, func() { batch.Set([]byte("a"), []byte{9}) })
-	require.Panics(t, func() { batch.Delete([]byte("a")) })
-	require.Panics(t, func() { batch.Write() })     // nolint: errcheck
-	require.Panics(t, func() { batch.WriteSync() }) // nolint: errcheck
-	batch.Close()
+	// trying to modify or rewrite a written batch should error, but closing it should work
+	require.Error(t, batch.Set([]byte("a"), []byte{9}))
+	require.Error(t, batch.Delete([]byte("a")))
+	require.Error(t, batch.Write())
+	require.Error(t, batch.WriteSync())
+	require.NoError(t, batch.Close())
 
 	// batches should write changes in order
 	batch = db.NewBatch()
-	batch.Delete([]byte("a"))
-	batch.Set([]byte("a"), []byte{1})
-	batch.Set([]byte("b"), []byte{1})
-	batch.Set([]byte("b"), []byte{2})
-	batch.Set([]byte("c"), []byte{3})
-	batch.Delete([]byte("c"))
-	err = batch.Write()
-	require.NoError(t, err)
-	batch.Close()
+	require.NoError(t, batch.Delete([]byte("a")))
+	require.NoError(t, batch.Set([]byte("a"), []byte{1}))
+	require.NoError(t, batch.Set([]byte("b"), []byte{1}))
+	require.NoError(t, batch.Set([]byte("b"), []byte{2}))
+	require.NoError(t, batch.Set([]byte("c"), []byte{3}))
+	require.NoError(t, batch.Delete([]byte("c")))
+	require.NoError(t, batch.Write())
+	require.NoError(t, batch.Close())
 	assertKeyValues(t, db, map[string][]byte{"a": {1}, "b": {2}})
 
-	// writing nil keys and values should be the same as empty keys and values
-	// FIXME CLevelDB panics here: https://github.com/jmhodges/levigo/issues/55
-	// if backend != CLevelDBBackend {
+	// empty and nil keys, as well as nil values, should be disallowed
 	batch = db.NewBatch()
-	batch.Set(nil, nil)
-	err = batch.WriteSync()
-	require.NoError(t, err)
-	assertKeyValues(t, db, map[string][]byte{"": {}, "a": {1}, "b": {2}})
+	err = batch.Set([]byte{}, []byte{0x01})
+	require.Equal(t, errKeyEmpty, err)
+	err = batch.Set(nil, []byte{0x01})
+	require.Equal(t, errKeyEmpty, err)
+	err = batch.Set([]byte("a"), nil)
+	require.Equal(t, errValueNil, err)
 
-	batch = db.NewBatch()
-	batch.Delete(nil)
-	err = batch.Write()
+	err = batch.Delete([]byte{})
+	require.Equal(t, errKeyEmpty, err)
+	err = batch.Delete(nil)
+	require.Equal(t, errKeyEmpty, err)
+
+	err = batch.Close()
 	require.NoError(t, err)
-	assertKeyValues(t, db, map[string][]byte{"a": {1}, "b": {2}})
-	// }
 
 	// it should be possible to write an empty batch
 	batch = db.NewBatch()
@@ -453,11 +342,11 @@ func testDBBatch(t *testing.T, db db.DB) {
 	batch.Close()
 	batch.Close()
 
-	// all other operations on a closed batch should panic
-	require.Panics(t, func() { batch.Set([]byte("a"), []byte{9}) })
-	require.Panics(t, func() { batch.Delete([]byte("a")) })
-	require.Panics(t, func() { batch.Write() })     // nolint: errcheck
-	require.Panics(t, func() { batch.WriteSync() }) // nolint: errcheck
+	// all other operations on a closed batch should error
+	require.Error(t, batch.Set([]byte("a"), []byte{9}))
+	require.Error(t, batch.Delete([]byte("a")))
+	require.Error(t, batch.Write())
+	require.Error(t, batch.WriteSync())
 }
 
 func verifyIterator(t *testing.T, itr db.Iterator, expected []int64, msg string) {
@@ -465,16 +354,6 @@ func verifyIterator(t *testing.T, itr db.Iterator, expected []int64, msg string)
 	for itr.Valid() {
 		key := itr.Key()
 		list = append(list, bytes2Int64(key))
-		itr.Next()
-	}
-	assert.Equal(t, expected, list, msg)
-}
-
-func verifyIteratorStrings(t *testing.T, itr db.Iterator, expected []string, msg string) {
-	var list []string
-	for itr.Valid() {
-		key := itr.Key()
-		list = append(list, string(key))
 		itr.Next()
 	}
 	assert.Equal(t, expected, list, msg)
