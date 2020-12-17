8d3e8a5db4d0c58ef5b0fc1b8906d44b75014ae7
Jun Kimura <junkxdev@gmail.com>
2020-12-17 17:04:53 +0900

    add ibc-transfer tests
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- x/ibc/testing/fabric.go
+++ x/ibc/testing/fabric.go
@@ -2,6 +2,7 @@ package testing
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	clienttypes "github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/types"
@@ -15,26 +16,40 @@ func queryEndorseCommitment(ctx contractapi.TransactionContextInterface, cc *cha
 	k := string(key)
 	parts := strings.Split(k, "/")
 
-	if strings.Contains(k, string(host.KeyClientState())) {
+	if strings.HasSuffix(k, "/"+string(host.KeyClientState())) {
 		clientID := parts[1]
 		return cc.EndorseClientState(ctx, clientID)
-	} else if strings.HasPrefix(k, string(host.KeyConnectionPrefix)) {
+	} else if strings.HasPrefix(k, string(host.KeyConnectionPrefix)+"/") {
 		connectionID := parts[1]
 		return cc.EndorseConnectionState(ctx, connectionID)
-	} else if strings.Contains(k, string(host.KeyConsensusStatesPrefix)+"/") {
+	} else if strings.HasPrefix(k, string(host.KeyClientStorePrefix)) && strings.Contains(k, string(host.KeyConsensusStatesPrefix)+"/") {
 		clientID := parts[1]
 		height, err := clienttypes.ParseHeight(parts[3])
 		if err != nil {
 			return nil, err
 		}
 		return cc.EndorseConsensusStateCommitment(ctx, clientID, height.VersionHeight)
-	} else if strings.HasPrefix(k, host.KeyChannelPrefix) {
+	} else if strings.HasPrefix(k, host.KeyChannelPrefix+"/") {
 		portID := parts[2]
 		channelID := parts[4]
 		return cc.EndorseChannelState(ctx, portID, channelID)
+	} else if strings.HasPrefix(k, host.KeyPacketCommitmentPrefix+"/") {
+		portID := parts[2]
+		channelID := parts[4]
+		seq, err := strconv.Atoi(parts[6])
+		if err != nil {
+			return nil, err
+		}
+		return cc.EndorsePacketCommitment(ctx, portID, channelID, uint64(seq))
+	} else if strings.HasPrefix(k, host.KeyPacketAckPrefix+"/") {
+		portID := parts[2]
+		channelID := parts[4]
+		seq, err := strconv.Atoi(parts[6])
+		if err != nil {
+			return nil, err
+		}
+		return cc.EndorsePacketAcknowledgement(ctx, portID, channelID, uint64(seq))
+	} else {
+		return nil, fmt.Errorf("unknown key: '%v'", k)
 	}
-
-	fmt.Println("queryEndorseCommitment:", k)
-
-	panic("not implemented error")
 }
